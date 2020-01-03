d247a6b30c1077826463764c5a3061f2ff6a3901
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-01-03 07:14:33 +0700

    Add Foo (unindexed, there's a Foo module in Test-Version) and Bar modules

---
+++ lib/Bar.pm
@@ -0,0 +1,14 @@
+package Bar;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A Bar module for testing
+
+=head1 DESCRIPTION
+
+Some distributions use C<Foo> as a random module name that is assumed to not
+exist. Well not so fast.
---
+++ lib/Foo.pm
@@ -0,0 +1,15 @@
+package
+    Foo;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A Foo module for testing
+
+=head1 DESCRIPTION
+
+Some distributions use C<Foo> as a random module name that is assumed to not
+exist. Well not so fast.
