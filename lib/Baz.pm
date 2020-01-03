8df8cd8a0a1507230c59fb37d55e20cf9307c139
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-01-03 07:25:51 +0700

    Add Baz (unindexed) and Foo::Bar::Baz (unindexed)

---
+++ lib/Baz.pm
@@ -0,0 +1,15 @@
+package
+    Baz;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A Baz module for testing
+
+=head1 DESCRIPTION
+
+Some distributions, in their tests, use C<Baz> as a random module name that is
+assumed to not exist. Well not so fast.
---
+++ lib/Foo/Bar/Baz.pm
@@ -0,0 +1,15 @@
+package
+    Foo::Bar::Baz;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A Foo::Bar::Baz module for testing
+
+=head1 DESCRIPTION
+
+Some distributions, in their tests, use C<Foo::Bar::Baz> as a random module name
+that is assumed to not exist. Well not so fast.
