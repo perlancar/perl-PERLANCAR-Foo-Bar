830d6b068084e9bec8dd0b42363e0001525e7ebd
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-01-03 07:06:35 +0700

    Initial commit

---
+++ lib/Foo/Bar.pm
@@ -0,0 +1,23 @@
+package Foo::Bar;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A Foo::Bar module for testing
+
+=head1 DESCRIPTION
+
+Some distributions use C<Foo::Bar> as a random module name that is assumed to
+not exist. Well not so fast.
+
+
+=head1 SEE ALSO
+
+L<https://rt.cpan.org/Ticket/Display.html?id=131338>
+
+L<https://rt.cpan.org/Ticket/Display.html?id=131249>
+
+L<https://rt.cpan.org/Ticket/Display.html?id=131207>
